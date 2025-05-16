7517f2580be445b8e05c6aa776caf5ab97833a95
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-16 12:46:07 +0300

    Exercise 3. - Format Person

---
+++ 01-02-intro-to-typescript-exercise/out/03.js
@@ -0,0 +1,7 @@
+"use strict";
+function formatPerson(personInfo) {
+    return `Hello, my name is ${personInfo[0]} and my age is ${personInfo[1]}`;
+}
+;
+console.log(formatPerson(['Ivan', 20, 'ivanov']));
+//# sourceMappingURL=03.js.map
