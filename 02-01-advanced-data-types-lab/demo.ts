21bf17c67c31d8e1f0b6b5cb65c51159b933c4bc
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-19 21:41:04 +0300

    Primitive type, interections

--- 02-01-advanced-data-types-lab/demo.ts
+++ 02-01-advanced-data-types-lab/demo.ts
@@ -9,3 +9,57 @@ function printGreeting(text: string | string[]): void { //union type
 
 printGreeting('Hello, Daniel');
 printGreeting(['Hello', 'its', 'me']);
+
+//------------------------------------------------------------
+// literal type and union type
+let passFailGrade: 'pass' | 'fail';
+passFailGrade = 'fail';
+console.log(passFailGrade);
+
+let numericGrade: 2 | 3 | 4 | 5 | 6;
+numericGrade = 3;
+numericGrade = 5;
+
+// primitive type
+type Id = number;
+
+function greet(humanId: Id){
+    return humanId;
+}
+
+// intersection
+type Person = {
+    id: Id;
+    firstName: string;
+    lastName: string;
+    age: number
+};
+
+type StudentProfile = {
+    school: string;
+    gpa: number;
+};
+
+type FullStudent = Person & StudentProfile;
+
+let danielPerson: FullStudent = {
+    id: 1,
+    firstName: 'Daniel',
+    lastName: 'Krastev',
+    age: 23,
+    school: 'MG',
+    gpa: 5.5,
+};
+
+function printStudentInfo(student: FullStudent) {
+    console.log(`Student ${student.firstName} ${student.lastName}, GPA: ${student.gpa}`);
+}
+
+let min4oPerson: Person = {
+    id: 2,
+    firstName: 'Min4o',
+    lastName: 'Min4ev',
+    age: 23,
+};
+
+printStudentInfo(danielPerson);
--- 02-01-advanced-data-types-lab/out/demo.js
+++ 02-01-advanced-data-types-lab/out/demo.js
@@ -9,4 +9,27 @@ function printGreeting(text) {
 }
 printGreeting('Hello, Daniel');
 printGreeting(['Hello', 'its', 'me']);
+//------------------------------------------------------------
+// literal type and union type
+let passFailGrade;
+passFailGrade = 'fail';
+console.log(passFailGrade);
+let numericGrade;
+numericGrade = 3;
+numericGrade = 5;
+function greet(humanId) {
+    return humanId;
+}
+function printStudentInfo(student) {
+    console.log(`Student ${student.firstName} ${student.lastName}, GPA: ${student.gpa}`);
+}
+let danielPerson = {
+    id: 1,
+    firstName: 'Daniel',
+    lastName: 'Krastev',
+    age: 23,
+    school: 'MG',
+    gpa: 5.5,
+};
+printStudentInfo(danielPerson);
 //# sourceMappingURL=demo.js.map
