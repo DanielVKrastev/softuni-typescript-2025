d92cd248a47f4487bc8cafb28c257ed834886d88
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-29 11:01:49 +0300

    Exercise 9. - Abstract Class

--- 03-02-oop-in-ts-exercise/out/08.js
+++ 03-02-oop-in-ts-exercise/out/08.js
@@ -10,6 +10,6 @@ class Book {
 }
 const book = new Book("1984", "George Orwell");
 console.log(`${book.title} by ${book.author}`);
-//book.title = "Brave New World";
+//book.title = "Brave New World"; //only read, no write
 //book.author = "Terry Pratchet";
 //# sourceMappingURL=08.js.map
