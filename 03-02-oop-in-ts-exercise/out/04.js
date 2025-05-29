08e4ab869b1ba01f4560533d9d38b95f9ba8a2d0
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-29 10:16:41 +0300

    Exercise 4. - Abstraction

---
+++ 03-02-oop-in-ts-exercise/out/04.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Dog {
+    makeSound() {
+        return 'Woof';
+    }
+}
+const dog = new Dog();
+console.log(dog.makeSound());
+//# sourceMappingURL=04.js.map
