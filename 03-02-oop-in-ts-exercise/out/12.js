e3c008730e0338d195bae5963f57e04c7da268a7
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-29 11:49:30 +0300

    Exercise 12. - Simple Task Tracker with Access Control

---
+++ 03-02-oop-in-ts-exercise/out/12.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Task {
+    title;
+    description;
+    completed = false;
+    _createdBy;
+    constructor(title, description, createdBy) {
+        this.title = title;
+        this.description = description;
+        this._createdBy = createdBy;
+    }
+    get createdBy() {
+        return this._createdBy;
+    }
+    toggleStatus() {
+        this.completed = !this.completed;
+    }
+    getDetails() {
+        return `Task: ${this.title} - ${this.description} - ${this.completed ? 'Completed' : 'Pending'}`;
+    }
+    static createSampleTasks() {
+        return [
+            new Task('Do Calc HW', 'complete all 200 problems', 'Daniel'),
+            new Task('Clean room', 'Sadnes', 'Daniel2'),
+        ];
+    }
+}
+const task1 = new Task("Complete homework", "Finish math exercises", "Charlie");
+task1.toggleStatus();
+console.log(task1.getDetails());
+const task2 = new Task("Clean room", "Clean the room", "Mary");
+console.log(task2.getDetails());
+const tasks = Task.createSampleTasks();
+tasks.forEach(task => console.log(task.getDetails()));
+//# sourceMappingURL=12.js.map
