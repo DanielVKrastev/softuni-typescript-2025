48c28575e242526c46d7c88e5d017e9095acce7a
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-06 11:28:00 +0300

    First demo user

---
+++ 05-01-ts-decorators-lab/01-user-demo.ts
@@ -0,0 +1,39 @@
+function LogClass(constructor: Function){
+    console.log(constructor);
+
+    console.log('------------------------------');
+    console.log('Class created!');
+    console.log('------------------------------');
+}
+
+@LogClass
+class User {
+    name: string;
+    age: number;
+
+    private _email!: string;
+
+    constructor(name: string, age: number, email: string){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    get email(){
+        return this._email;
+    }
+
+    set email(val: string){
+        this._email = val;
+    }
+
+    getInfo(condensed: boolean): string{
+        return condensed ? `Person ${this.name}` : `Person ${this.name} is ${this.age} years old with email: ${this.email}`;
+    }
+}
+
+@LogClass
+class Employee {
+    name: string = 'Daniel';
+    salary: number = 2000;
+}
