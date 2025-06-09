986d4a618627f1e0e5fc47d8460812042b68bdf8
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-09 22:59:25 +0300

    Created APIs

---
+++ 06-workshop/src/services/api.ts
@@ -0,0 +1,42 @@
+export class ApiService<T> {
+    protected baseServiceUrl: string;
+
+    constructor(url: string) {
+        this.baseServiceUrl = url;
+    }
+
+    async getAll(): Promise<T[]> {
+        const res = await fetch(this.baseServiceUrl);
+        return await res.json();
+    }
+
+    async create(item: T): Promise<T> {
+        const res = await fetch(this.baseServiceUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(item),
+        });
+
+        return await res.json();
+    }
+
+    async update(id: number, item: T): Promise<T> {
+        const res = await fetch(`${this.baseServiceUrl}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(item),
+        });
+
+        return await res.json();
+    }
+
+    async delete(id: number): Promise<void> {
+        await fetch(`${this.baseServiceUrl}/${id}`, {
+            method: 'DELETE',
+        });
+    }
+}
