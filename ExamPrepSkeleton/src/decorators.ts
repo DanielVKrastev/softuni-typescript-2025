4fd6db4b0ea0e0ebc127593c3144b49a9c1eeaa0
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-12 20:15:49 +0300

    Decorators 2 and 3

--- ExamPrepSkeleton/src/decorators.ts
+++ ExamPrepSkeleton/src/decorators.ts
@@ -3,8 +3,8 @@ export function decorator1<T extends new(...args: any[]) => {}>(constructor: T){
         protected _offset: number = 3;
     }
 }
-export function decorator2(){}
-export function decorator3(){}
+export function decorator2(target: object, methodName: string, desciptor: PropertyDescriptor){}
+export function decorator3(target: object, methodName: string, desciptor: PropertyDescriptor){}
 export function decorator4<T extends abstract new(...args: any[]) => {}>(constructor: T){
     abstract class ExtendedPartialMessageEncoder extends constructor {
         public static forbiddenSymbols: string[] = ['_', ',', '.', '!', '?', '-', ';', ' ',  '"', '\''];
