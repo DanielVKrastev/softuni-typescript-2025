85fe26a9dc17687a687b2439de131e1c9ecdb342
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-12 18:44:23 +0300

    Structure 3.
    3.The LanguageMessageEncoder class should be a generic class accepting 2 generic parameters - a Language and a Cipher compatible with that language.

--- ExamPrepSkeleton/src/languageMessageEncoder.ts
+++ ExamPrepSkeleton/src/languageMessageEncoder.ts
@@ -1,1 +1,8 @@
-export class LanguageMessageEncoder {}
+import { Cipher } from "./contracts/cipher";
+import { PartialMessageEncoder } from "./contracts/implemented/partialMessageEncoder";
+import { Language } from "./contracts/language";
+import { MessageEncoder } from "./contracts/messageEncoder";
+
+export class LanguageMessageEncoder<TLanguage extends Language, TCipher extends Cipher<TLanguage>> extends PartialMessageEncoder implements MessageEncoder{
+    // constructor must work with languange and cipher
+}
