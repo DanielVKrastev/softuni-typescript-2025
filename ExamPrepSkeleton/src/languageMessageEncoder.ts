06c9ac827415b03b7ef1a361d877c0c192c271ec
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-12 18:49:12 +0300

    Tasks: LanguageMessageEncoder
    •	Keep track of the total number of sucessfully encoded and decoded characters.
    Structure 2. The LanguageMessageEncoder class should only allow to be initialized with objects compatible with the Language and Cipher interfaces.

--- ExamPrepSkeleton/src/languageMessageEncoder.ts
+++ ExamPrepSkeleton/src/languageMessageEncoder.ts
@@ -4,5 +4,10 @@ import { Language } from "./contracts/language";
 import { MessageEncoder } from "./contracts/messageEncoder";
 
 export class LanguageMessageEncoder<TLanguage extends Language, TCipher extends Cipher<TLanguage>> extends PartialMessageEncoder implements MessageEncoder{
-    // constructor must work with languange and cipher
+    private encodedCount = 0;
+    private decodedCount = 0;
+
+    constructor(language: TLanguage, cipher: TCipher) {
+        super(language, cipher);
+    }
 }
